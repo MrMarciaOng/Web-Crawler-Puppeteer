15cf3df26d00ecb9d78bb645c4bf007f21d6d337
Marcia AMD PC <mrmarciaong@gmail.com>
2019-02-19 01:11:55 +0800

    tbc

--- test.js
+++ test.js
@@ -2,51 +2,86 @@ const puppeteer = require('puppeteer');
 const request = require('request-promise');
 var fs = require('fs');
 var XLSX = require('xlsx');
-
+var sleep = require('system-sleep');
 (async () => {
+
     try{
         // Viewport && Window size
-        const width = 1980
+        const width = 1080
         const height = 1080
 
         const browser = await puppeteer.launch({
             headless: false,
             args:[
-                '--start-maximized' 
+               
+                '--ignore-certificate-errors',
+                '--ignore-certificate-errors-spki-list '
              ]
         } );
-        const page = await browser.newPage();
-        var workbook = XLSX.readFile('./excel_anno_40sheets_JiewenExample.xlsx');
+        var sheetToProcess = 0
+        var page = await browser.newPage();
+        var workbook = XLSX.readFile('./astar.xlsx');
         var sheet_name_list = workbook.SheetNames;
-        console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[3]]));
-        var temp = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[3]])
+        console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[sheetToProcess]]));
+        var temp = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[sheetToProcess]])
         await page.setViewport({ width, height })
 
         for(let x = 0; x < temp.length ; x++){
-            await page.goto('https://www.wikidata.org/w/index.php?search');
-            await page.type('input[name="search"]', temp[x].token) //document queryselector finding input tag with attribute name = "email"
-            await page.click('button[type="submit"]')
-            await page.waitForNavigation()
-            var listofitems = await page.$$('.mw-search-result-heading > a')
-            console.log(listofitems)
-            for (let y = 0 ; y < listofitems.length ; y++){
+            console.log("processing "+ x +" out of "+temp.length)
+            if(x==0 ){
+                await page.goto('https://www.wikidata.org/w/index.php?search');
+            }
+            if(x%20 ==0){
+                console.log("here")
+                await page.close();
+                page = await browser.newPage();
+                await page.goto('https://www.wikidata.org/w/index.php?search');
+            }
+            if(  temp[x].token != "null"){
 
-                console.log(await listofitems[y].$eval('[attr=title]'))
+                
+                await page.type('input[name="search"]', temp[x].token) 
+                sleep(0.5*1000); 
+                await page.click('button[type="submit"]')
+                await page.waitForNavigation()
+                var listofitems = await page.$$eval('#mw-content-text > div.searchresults > ul > li > div.mw-search-result-heading > a'
+                 ,as => as.map(a =>({href:a.href , title : a.title})))
+                if(listofitems.length > 0){
+                    for (let y = 0 ; y < 4 ; y++){
+                        if(listofitems[y].title.match(/[a-zA-Z ]+/).toString().replace(/^[ ]+|[ ]+$/g,'')== temp[x].token){
+                            if(listofitems[y].href.match("https://www.wikidata.org/wiki/(.*)")[1].toString().substring(0, 1)!='P')
+                            temp[x].Wikidata = "B-"+listofitems[y].href.match("https://www.wikidata.org/wiki/(.*)")[1]
+                        }
+                        
+                    }
+                }
+    
+                await page.click('input[name="search"]', {clickCount: 3})
+                if(x%5 ==0){
+                    console.log("sleeping")
+                    sleep(4*1000); 
+                    
+                    
+                }
+                else if(x%2 ==0){
+                    console.log("sleeping")
+                    sleep(3*1000); 
+
+                }
             }
 
         }
-        
-
+        var ws = XLSX.utils.json_to_sheet(temp);
+        var wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "report");
+        XLSX.writeFile(wb, 'out '+ (Date.now() % 171761)+'.xlsx');
         
      
         
         
-        await page.goto('url');
 
-        await page.type('input[name="startDate"]', '01-09-2018') //form text input
-        await page.type('input[name="endDate"]', '15-09-2018')
         await page.setRequestInterception(true); // Allow request interception before click to download file
-        page.click('button#submitButton');
+        // page.click('button#submitButton');
 
         const xRequest = await new Promise(resolve => { //download method
             page.on('request', request => {
@@ -82,3 +117,5 @@ var XLSX = require('xlsx');
         console.log(e)
     }
 })();
+
+
